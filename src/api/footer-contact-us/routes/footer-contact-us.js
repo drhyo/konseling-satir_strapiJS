3a987c1ea878468ecd9ca9aacc3b11bedb10aee2
yudha <127107368+yudhajaya75@users.noreply.github.com>
2023-10-17 15:58:50 +0700

    update env (#4)

--- src/api/footer-contact-us/routes/footer-contact-us.js
+++
@@ -1,7 +0,0 @@
-/**
- * footer-contact-us router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::footer-contact-us.footer-contact-us');
