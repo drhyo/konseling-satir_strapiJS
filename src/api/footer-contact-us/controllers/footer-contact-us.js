3b0d9406856d2d1845cc8c65685054e56300daf8
yudha <yudajaya75@gmail.com>
2023-10-17 15:48:37 +0700

    update env

--- src/api/footer-contact-us/controllers/footer-contact-us.js
+++
@@ -1,7 +0,0 @@
-/**
- * footer-contact-us controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::footer-contact-us.footer-contact-us');
--- src/api/footer-contact-us/services/footer-contact-us.js
+++
@@ -1,7 +0,0 @@
-/**
- * footer-contact-us service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::footer-contact-us.footer-contact-us');
